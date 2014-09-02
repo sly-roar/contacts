c6be46a6f9fa956b489b7aebb242a2660af8c329
Aleksey Zolotukhin <jakal66@gmail.com>
2014-09-02 11:41:55 +0600

    final commit

--- hostco/settings.py
+++ hostco/settings.py
@@ -59,12 +59,25 @@ WSGI_APPLICATION = 'hostco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'hostco',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'django',
+            'PASSWORD': 'django123',
+            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '',                      # Set to empty string for default.
+        }
     }
-}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -82,13 +95,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_PATH = os.path.join(PROJECT_PATH,'static')
+#STATIC_PATH = os.path.join(PROJECT_PATH,'static')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    STATIC_PATH,
-)
+#STATICFILES_DIRS = (
+#    STATIC_PATH,
+#)
+
+STATIC_ROOT = "/home/jakal/django_project/hostco/static/"
 
 #TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 
